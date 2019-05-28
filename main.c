ef1c6c59cec84a6bd176e9384685d962d01e2c12
Mafranpe <51120056+Mafranpe@users.noreply.github.com>
2019-05-28 14:18:51 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-double SumasParciales(double n){
 
+double SumasParciales(double n)
+{
     if (n==0) return 0;
-
-    return ((1.0/n)+SumasParciales(n-1));
-
+    double j=0;
+    for (int i=1; i<n; i++)
+    {
+        j=j+(1.0/i);
+    }
+    return j;
 }
 
-
-int CalculaRiemann(int* ptr){
+int CalculaRiemann(int* ptr)
+{
     int i;
-    for( i=0;SumasParciales(i)<*ptr;i++){
-            printf("%d ",i);
-            printf("%f\n",SumasParciales(i));
+    for( i=0; SumasParciales(i)<*ptr; i++)
+    {
+        printf("%d ",i);
+        printf("%f\n",SumasParciales(i));
     }
     printf("%f\n",SumasParciales(i));
     return i;
 
 }
 
-
-
-
-int main(){
-
-int p;
-scanf("%d",&p);
-printf("%d",CalculaRiemann(&p));
+int main()
+{
+    int p;
+    scanf("%d",&p);
+    printf("%d",CalculaRiemann(&p));
 }
